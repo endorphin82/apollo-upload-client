15b13f8e3dedc444fc063aad6eab2b9028bb1219
endorphin82 <endorphin82@gmail.com>
2020-06-19 18:51:52 +0300

    adding hook

--- src/App.js
+++ src/App.js
@@ -4,19 +4,29 @@ import gql from 'graphql-tag';
 import Dropzone from 'react-dropzone';
 
 const uploadFileMutation = gql`
-  mutation($file: Upload!) {
-    uploadFile(file: $file){
-     name
+    mutation($file: Upload!) {
+        uploadFile(file: $file){
+            filename
+        }
     }
-  }
 `;
 
-export default () => (
-  <Mutation mutation={uploadFileMutation}>
-    {mutate => (
-      <Dropzone onDrop={([file]) => {console.log(file); return mutate({ variables: { file } })}}>
-        <p>Try dropping some files here, or click to select files to upload.</p>
-      </Dropzone>
-    )}
-  </Mutation>
-);
+export default () => {
+  return (
+    <Mutation mutation={uploadFileMutation}>
+      {mutate => (
+        <Dropzone
+          inputProps={{ name: 'file' }}
+          onDrop={
+            ([file]) => {
+              console.log(file);
+              return mutate({ variables: { file } });
+            }
+          }
+        >
+          <p>Try dropping some files here, or click to select files to upload.</p>
+        </Dropzone>
+      )}
+    </Mutation>
+  );
+};
---
+++ src/services/FileUploadService.js
@@ -0,0 +1,23 @@
+import http from "../http-common";
+
+const upload = (file, onUploadProgress) => {
+  let formData = new FormData();
+console.log("formData", formData)
+  formData.append("file", file);
+  console.log("++++", file)
+    return http.post("/", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+    onUploadProgress,
+  });
+};
+
+const getFiles = () => {
+  return http.get("/files");
+};
+
+export default {
+  upload,
+  getFiles,
+};
